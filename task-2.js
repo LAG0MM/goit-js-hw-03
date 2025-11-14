4e2ee7c047bcdfa917b7152446a77a8f5e31c800
LAG0MM <166709774+LAG0MM@users.noreply.github.com>
2025-11-14 22:32:41 +0100

    1

---
+++ task-2.js
@@ -0,0 +1,15 @@
+function makeArray(firstArray, secondArray, maxlength) { 
+    const couple = [...firstArray, ...secondArray]
+
+    if (couple.length > maxlength) { 
+        return couple.slice(0, maxlength)
+    }
+    return couple
+}
+
+console.log(makeArray(["Mango", "Poly"], ["Ajax", "Chelsea"], 3)); // ["Mango", "Poly", "Ajax"]
+console.log(makeArray(["Mango", "Poly", "Houston"], ["Ajax", "Chelsea"], 4)); // ["Mango", "Poly", "Houston", "Ajax"]
+console.log(makeArray(["Mango"], ["Ajax", "Chelsea", "Poly", "Houston"], 3)); // ["Mango", "Ajax", "Chelsea"]
+console.log(makeArray(["Earth", "Jupiter"], ["Neptune", "Uranus"], 2)); // ["Earth", "Jupiter"]
+console.log(makeArray(["Earth", "Jupiter"], ["Neptune", "Uranus"], 4)); // ["Earth", "Jupiter", "Neptune", "Uranus"]
+console.log(makeArray(["Earth", "Jupiter"], ["Neptune", "Uranus", "Venus"], 0)); // []
---
+++ task-3.js
@@ -0,0 +1,15 @@
+function filterArray(numbers, value) {
+    const finish = []
+
+    for (const num of numbers) {
+        if (num > value)
+            finish.push(num)
+    }
+    return finish
+}
+
+console.log(filterArray([1, 2, 3, 4, 5], 3)); // [4, 5]
+console.log(filterArray([1, 2, 3, 4, 5], 4)); // [5]
+console.log(filterArray([1, 2, 3, 4, 5], 5)); // []
+console.log(filterArray([12, 24, 8, 41, 76], 38)); // [41, 76]
+console.log(filterArray([12, 24, 8, 41, 76], 20)); // [24, 41, 76]
